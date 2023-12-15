e675fb5dfe896f97cd4efdaa63085547f28c5b95
rlanrid <rladnrider77@gmail.com>
2023-12-15 10:15:53 +0900

    세팅

---
+++ client/src/components/layout/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer id='footer' role='contentinfo'>
+            <a href='mailto:rladnrider77@gmail.com' rel='noopenner noreferrer'>
+                rlanrider@gmail.com
+            </a>
+        </footer>
+    )
+}
+
+export default Footer
